275adb58ad4f1a6a47332c40a30ea5375504064d
roboanonymous <jsamyak591@gmail.com>
2023-01-15 16:12:37 +0530

    first

--- src/main/java/com/db/DBconnect.java
+++ src/main/java/com/db/DBconnect.java
@@ -1,6 +1,22 @@
 package com.db;
 import java.sql.Connection;
+import java.sql.DriverManager;
 public class DBconnect {
 	private Connection conn;
 	
+	public Connection getConn() {
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel", "root", "samyak123");
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
 }
